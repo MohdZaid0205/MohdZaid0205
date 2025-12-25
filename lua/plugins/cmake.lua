d0eb80c3b7d8102c25d29a37dbba11eba913c83e
Mohd Zaid <mdzaid0205@gmail.com>
2025-12-25 16:18:53 +0000

    change shortcuts

--- lua/plugins/cmake.lua
+++ lua/plugins/cmake.lua
@@ -3,13 +3,16 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
     cmake_build_directory = "build/${variant:buildType}", 
+    cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
+    cmake_soft_link_compile_commands = true, -- Create a link to compile_commands.json (needed for LSP)
+    compile_commands_directory = "./",       -- But keep the link in root so clangd finds it
   },
   keys = {
     -- F5: Build
-    { "<F5>", "<cmd>CMakeBuild<CR>", desc = "Build Project" },
-    { "<S-F5>", "<cmd>CMakeSelectBuildType<CR>", desc = "Select Build Type" },
-    { "<C-F5>", "<cmd>CMakeSelectConfigurePreset<CR>", desc = "Select CMake Preset" },
-    { "<F6>", "<cmd>CMakeInstall<CR>", desc = "Install Target" },
+    { "<leader>bb", "<cmd>CMakeBuild<CR>", desc = "Build Project" },
+    { "<leader>bt", "<cmd>CMakeSelectBuildType<CR>", desc = "Select Build Type" },
+    { "<leader>bc", "<cmd>CMakeSelectConfigurePreset<CR>", desc = "Select CMake Preset" },
+    { "<leader>bi", "<cmd>CMakeInstall<CR>", desc = "Install Target" },
 
   },
 }
