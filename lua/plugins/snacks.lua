1b10e767fe80f1b1c32c53454cb92139bc9a7dd7
Mohd Zaid <mdzaid0205@gmail.com>
2026-05-11 20:14:55 +0530

    update snacks plugin configuration with dashboard sections and add lazy-lock.json

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -30,7 +30,15 @@ return {
       -- }
     },
 
-    dashboard = { enabled = true },
+    dashboard = { 
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
     explorer = { 
       enabled = true,
       layout = {
