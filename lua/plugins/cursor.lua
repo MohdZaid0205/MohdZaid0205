44e955de7a5355dba9a17e71cf267a66596ada7c
Mohd Zaid <mdzaid0205@gmail.com>
2026-05-13 06:45:22 +0000

    added smear cirsor effect

---
+++ lua/plugins/cursor.lua
@@ -0,0 +1,7 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    smear_between_neighbor_lines = true, -- The "flowy" look
+    cursor_color = "#d3cdc3",           -- Match your theme
+  },
+}
